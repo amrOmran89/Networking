efa52e1848e1699445d53e2ce659fc2a8dc2870d
Amr Omran <amr.omran@exxeta.com>
2022-04-16 21:53:37 +0200

    removed some unused code

--- Sources/Networking/NetworkService.swift
+++ Sources/Networking/NetworkService.swift
@@ -4,25 +4,7 @@ import Combine
 public class NetworkService<R: Router>: Requestable {
     
     public init() {}
-    
-    public func fetch<T: Codable>(url: URL, decoder: JSONDecoder = JSONDecoder(), compilation: @escaping (Result<T, Error>) -> Void) {
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let res = response as? HTTPURLResponse, res.statusCode != 200  {
-                return compilation(.failure(NetworkingError.serverResponse(res.statusCode)))
-            }
-            guard let data = data else {
-                return compilation(.failure(NetworkingError.dataNotFound))
-            }
-            do {
-                let json = try decoder.decode(T.self, from: data)
-                compilation(.success(json))
-            } catch {
-                return compilation(.failure(error))
-            }
-            
-        }.resume()
-    }
+
     
     public func fetch<T: Codable>(router: R, decoder: JSONDecoder = JSONDecoder(), compilation: @escaping (Result<T, Error>) -> Void) where R: Router {
         do {
@@ -51,20 +33,6 @@ public class NetworkService<R: Router>: Requestable {
         }
     }
     
-    public func fetch<T: Codable>(url: URL, decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<T, Error> {
-        URLSession
-            .shared
-            .dataTaskPublisher(for: url)
-            .tryMap { (data: Data, res: URLResponse) in
-                if let response = res as? HTTPURLResponse, response.statusCode != 200 {
-                    throw NetworkingError.serverResponse(response.statusCode)
-                }
-                return data
-            }
-            .decode(type: T.self, decoder: decoder)
-            .eraseToAnyPublisher()
-    }
-    
     
     public func fetch<T: Codable>(router: R, decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<T, Error> where R: Router {
         
@@ -81,21 +49,6 @@ public class NetworkService<R: Router>: Requestable {
                 .eraseToAnyPublisher()
         } catch {
             return Fail(error: error).eraseToAnyPublisher()
-        }
-    }
-    
-    
-    public func fetch<T: Codable>(url: URL, decoder: JSONDecoder = JSONDecoder()) async throws -> T {
-        do {
-            let (data, res) = try await URLSession.shared.data(from: url, delegate: nil)
-            if let response = res as? HTTPURLResponse, response.statusCode != 200 {
-                throw NetworkingError.serverResponse(response.statusCode)
-            }
-            
-            let json = try decoder.decode(T.self, from: data)
-            return json
-        } catch {
-            throw error
         }
     }
     
